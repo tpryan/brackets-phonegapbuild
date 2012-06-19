1cf101dd18b6dadb749d12b0dd6ad8755181ae89
Terrence Ryan <terry@terrenceryan.com>
2012-06-19 14:30:25 -0400

    Login now works.  No need to hardcode password.

--- main.js
+++ main.js
@@ -6,15 +6,41 @@ define(function (require, exports, module) {
     'use strict';
 
     var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus");
+        Menus          = brackets.getModule("command/Menus"),
+        Commands                = brackets.getModule("command/Commands"),
+        EditorManager           = brackets.getModule("editor/EditorManager"),
+        DocumentManager         = brackets.getModule("document/DocumentManager");
+
+
+    // First, register a command - a UI-less object associating an id to a handler
+    var PG_LIST = "PhoneGap.list";
+    CommandManager.register("List Build Projects", PG_LIST, handlePGList);
+
+    var PG_LOGIN = "PhoneGap.login";
+    CommandManager.register("Login", PG_LOGIN, handlePGLogin);
+
+    var PG_LOGOUT = "PhoneGap.logout";
+    CommandManager.register("Logout", PG_LOGOUT, handlePGLogout);
+
+
+    var menu;
+    menu = Menus.addMenu("PhoneGap", "tpryan.phonegap.phonegap");
+    menu.addMenuItem(PG_LIST);
+    console.log("Menu:");
+    console.log(menu);
+
+
+
 
     // Local modules
     require('phonegapbuild');    
     var phonegapbuild = new PhoneGapBuild();
-    //hardcode for now.  Eventually, we'll save the token in local storage. 
-    phonegapbuild.getToken("", "");   
-
+    phonegapbuild.addListener("initialized",  handlePGInitialize);
+    phonegapbuild.initialize();
     
+    
+
+
     // Function to run when the menu item is clicked
     function handlePGList() {
         var list = "";
@@ -22,19 +48,79 @@ define(function (require, exports, module) {
             list += phonegapbuild.list[i].title + ", ";
         }
 
-
         window.alert(list);
     }
-    
-    
-    // First, register a command - a UI-less object associating an id to a handler
-    var PG_LIST = "PhoneGap.list";
-    CommandManager.register("List Build Projects", PG_LIST, handlePGList);
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu;
-    menu = Menus.addMenu("PhoneGap", "tpryan.phonegap.phonegap");
-    menu.addMenuItem(PG_LIST);
+    function getPGList(){
+        phonegapbuild.getList()
+    }
+
+    function handlePGLogin(){
+        console.log("show Login Form");
+        toggleLoginDisplay();
+    }
+
+    function handlePGLogout(){
+        phonegapbuild.logout();
+        menu.addMenuItem(PG_LOGIN);
+        //menu.removeMenuItem(PG_LOGOUT);
+    }
+
+    function toggleLoginDisplay(){
+      var $pgLogin = $("#pg-login");
+        
+      if ($pgLogin.css("display") === "none") {
+          $pgLogin.show();
+      } else {
+          $pgLogin.hide();
+      }
+      EditorManager.resizeEditor();
+    }
+
+    function doLogin(){
+        event.preventDefault();
+        var $username = $('#username').val(); 
+        var $password = $('#password').val();
+        phonegapbuild.getToken($username, $password); 
+    }
+
+    function handlePGInitialize(e){
+
+        $('.content').append('  <div id="pg-login" class="bottom-panel">' + 
+                                    '  <div class="toolbar simple-toolbar-layout">' + 
+                                    '    <div class="title">PhoneGap Build</div><a href="#" class="close">&times;</a>' +
+                                    '  </div>' +
+                                    '    <form>' +
+                                    '        <label for="username">Username:</label> <input id="username" type="email" name="username" /><br />' +
+                                    '        <label for="password">Password:</label> <input id="password" type="password" name="password" /><br />' +
+                                    '        <input id="loginsubmit" type="submit" class="btn" name="sumbit" value="Login!" /><br />' +
+                                    '    </form>' +  
+                                '</div>');
+
+        $('#pg-login input').css("float", "none");
+
+        $('#pg-login .close').click(function () {
+            toggleLoginDisplay();
+        });
+        $('#loginsubmit').click(function () {
+            doLogin();
+        });
+
+        
+        EditorManager.resizeEditor();
+        console.log(e);
+        if (e.detail.tokenDefined == true){
+            getPGList();
+            console.log("Token was in localstorage");
+            menu.addMenuItem(PG_LOGOUT);
+        }
+        else{
+            console.log("Token was NOT in localstorage");
+            menu.addMenuItem(PG_LOGIN);
+            phonegapbuild.addListener("tokenloaded",  getPGList);
+
+        }
+    }
+
     
 });
--- phonegapbuild.js
+++ phonegapbuild.js
@@ -5,16 +5,42 @@ var PhoneGapBuild = function() {
 	var URL_TOKEN = URL_BASE + "/token";
 	var URL_LIST = URL_BASE + "/api/v1/apps";
 
-	var self = this;
+	
+  this._listeners = {};
 
 	this.token = "";
 	this.list ="List of PhoneGap Build projects."
+  this.intialized = false;
 	this.getToken = getToken; 
-	this.getList = getList; 
+	this.getList = getList;
+  this.addListener = addListener;
+  this.initialize = initialize;
+  this.logout = logout;    
 
+  var self = this;
+
+  function initialize(){
+    var token = localStorage.getItem("token");
+    var tokenDefined = false;
+
+    if (token != ""){
+      tokenDefined = true;
+      setToken(token);
+      self.initialized = true;
+    }
+
+    var myEvent = new CustomEvent("initialized", {
+      detail: {
+        tokenDefined: tokenDefined
+      }
+    });
+    fire(myEvent);
+
+  }
 
 	function setToken(token){
 		self.token = token;
+    localStorage.setItem('token', token);
 		console.log(this);
 	}
 
@@ -38,37 +64,102 @@ var PhoneGapBuild = function() {
 	}
 
 	function handleTokenSuccess(response, status, jqXHR){
-		console.log("Token Retreived");
-      	console.log(response);
-      	setToken(response.token);
-      	getList();
+    console.log("Token Retreived");
+    console.log(response);
+    setToken(response.token);
+
+    var myEvent = new CustomEvent("tokenloaded", {
+      detail: {
+        token: response.token
+      }
+    });
+    fire(myEvent);
+  }
+
+  function logout(){
+    setToken("");
+    tokenDefined = true;
+    self.initialized = true;
+
+    var myEvent = new CustomEvent("logout", {});
+    fire(myEvent);
+  }
+
+
+  function errorHandler(error){
+    console.log("Login Error");
+    console.log(error.responseText);
+  }
+
+  function getList(){
+  	$.ajax({
+        url: URL_LIST + "?auth_token=" + this.token,
+        success: handleListSuccess,
+        dataType: 'jsonp', 
+        type:"get",
+        error: errorHandler,
+        cache:false,
+        crossDomain:true
+      });
+  }
+
+  function handleListSuccess(response, status, jqXHR){
+    console.log("List Retreived");
+    console.log(response.apps);
+    setList(response.apps);
+
+    var myEvent = new CustomEvent("listloaded", {
+      detail: {
+        list: response.apps
+      }
+    });
+    fire(myEvent);
+  }
+
+
+  function addListener(type, listener){
+        if (typeof this._listeners[type] == "undefined"){
+            this._listeners[type] = [];
+        }
+
+        this._listeners[type].push(listener);
     }
 
-    function errorHandler(error){
-      console.log("Login Error");
-      console.log(error.responseText);
+    function fire(event){
+        if (typeof event == "string"){
+            event = { type: event };
+        }
+        if (!event.target){
+            event.target = self;
+        }
+
+        if (!event.type){  //falsy
+            throw new Error("Event object missing 'type' property.");
+        }
+
+        if (self._listeners[event.type] instanceof Array){
+            var listeners = self._listeners[event.type];
+            for (var i=0, len=listeners.length; i < len; i++){
+                listeners[i].call(self, event);
+            }
+        }
     }
 
-    function getList(){
-    	$.ajax({
-          url: URL_LIST + "?auth_token=" + this.token,
-          success: handleListSuccess,
-          dataType: 'jsonp', 
-          type:"get",
-          error: errorHandler,
-          cache:false,
-          crossDomain:true
-        });
+    function removeListener(type, listener){
+        if (this._listeners[type] instanceof Array){
+            var listeners = this._listeners[type];
+            for (var i=0, len=listeners.length; i < len; i++){
+                if (listeners[i] === listener){
+                    listeners.splice(i, 1);
+                    break;
+                }
+            }
+        }
     }
 
-    function handleListSuccess(response, status, jqXHR){
-	    console.log("List Retreived");
-	    console.log(response.apps);
-	    setList(response.apps);
-    }
+    
 
-
-
+    
 
 
 }	
